51518fd48d3dd2c34545c07784a6c47333d1b3c8
Zachary Gouker <zachgouker@gmail.com>
2019-11-02 12:42:21 -0400

    Added script to pull down games, and moved CSVs to separate folder

--- pull_teams.py
+++ pull_teams.py
@@ -7,10 +7,10 @@ def reindex_from_1(df):
 team_request_get = requests.get(url="https://api.collegefootballdata.com/teams?year=2019")
 team_df =  pandas.io.json.json_normalize(team_request_get.json())
 fbs_team_df = team_df[team_df.conference.notnull()]
-fbs_team_df.to_csv("teams.csv")
+fbs_team_df.to_csv("data/teams.csv")
 fbs_team_df = reindex_from_1(fbs_team_df)
-fbs_team_df.to_csv("fbs_teams_long.csv")
+fbs_team_df.to_csv("data/fbs_teams_long.csv")
 fbs_team_df['logo'] = fbs_team_df.logos.apply(lambda x: x[0])
 fbs_team_df['logo']
 fbs_team_df = fbs_team_df[['logo','school','mascot','conference','id']]
-fbs_team_df.to_csv("fbs_teams_short.csv")
+fbs_team_df.to_csv("data/fbs_teams_short.csv")
